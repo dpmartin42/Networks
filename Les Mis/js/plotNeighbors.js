f32e37ec0536b71c84711c122efc4cea58b60122
Daniel Martin <dpmartin42@gmail.com>
2014-08-07 20:00:41 -0400

    add name search highlighting

--- Les Mis/js/plotNeighbors.js
+++ Les Mis/js/plotNeighbors.js
@@ -1,51 +1,49 @@
+// When a node is clicked, we check for each node
+// if it is a neighbor of the clicked one. If not,
+// we set its color as grey, and else, it takes its
+// original color.
+// We do the same for the edges, and we only keep
+// edges that have both extremities colored.
 
-	
-	
+s.bind('clickNode', function(e) {
+    var nodeId = e.data.node.id,
+        toKeep = s.graph.neighbors(nodeId);
+    toKeep[nodeId] = e.data.node;
 
-    // When a node is clicked, we check for each node
-    // if it is a neighbor of the clicked one. If not,
-    // we set its color as grey, and else, it takes its
-    // original color.
-    // We do the same for the edges, and we only keep
-    // edges that have both extremities colored.
-    s.bind('clickNode', function(e) {
-        var nodeId = e.data.node.id,
-            toKeep = s.graph.neighbors(nodeId);
-    	toKeep[nodeId] = e.data.node;
+    s.graph.nodes().forEach(function(n) {
+        if (toKeep[n.id])
+            n.color = n.originalColor;
+        else
+            n.color = '#eee';
+    });
 
-    	s.graph.nodes().forEach(function(n) {
-        	if (toKeep[n.id])
-            	n.color = n.originalColor;
-          	else
-            	n.color = '#eee';
-    	});
-
-        s.graph.edges().forEach(function(e) {
-          if (toKeep[e.source] && toKeep[e.target])
-            e.color = e.originalColor;
-          else
+    s.graph.edges().forEach(function(e) {
+        if (toKeep[e.source] && toKeep[e.target])
+        	e.color = e.originalColor;
+        else
             e.color = '#eee';
-        });
+    });
 
-        // Since the data has been modified, we need to
-        // call the refresh method to make the colors
-        // update effective.
-        s.refresh();
+    // Since the data has been modified, we need to
+    // call the refresh method to make the colors
+    // update effective.
+    s.refresh();
         
-      });
+    });
 
-      // When the stage is clicked, we just color each
-      // node and edge with its original color.
-      s.bind('clickStage', function(e) {
-      	s.graph.nodes().forEach(function(n) {
-          n.color = n.originalColor;
-        });
+// When the stage is clicked, we just color each
+// node and edge with its original color.
 
-        s.graph.edges().forEach(function(e) {
-          e.color = e.originalColor;
-        });
+s.bind('clickStage', function(e) {
+	s.graph.nodes().forEach(function(n) {
+    	n.color = n.originalColor;
+    });
 
-        // Same as in the previous event:
-        s.refresh();
-      });
+    s.graph.edges().forEach(function(e) {
+        e.color = e.originalColor;
+    });
+
+    // Same as in the previous event:
+    s.refresh();
+});
 
